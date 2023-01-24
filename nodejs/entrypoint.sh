9fd7d9e70d937826adbc9becb0c6def4dccd11b8
Jpuf <jpuf@jpuf.xyz>
2023-01-24 14:15:44 +0000

    updated nodejs to include latest versions

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/container
 
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
 node -v
 
